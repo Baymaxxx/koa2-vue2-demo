ba96ac07d4b648c2a40b39cfff21609a2f0bedcb
baymaxxx <81243232@qq.com>
2017-03-16 15:07:23 +0800

    Initial commit

---
+++ src/components/Login.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="login">
+    <el-row class="content">
+      <el-col :xs="24" :sm="{span: 6,offset: 9}">
+        <h2 class="title">欢迎登录</h2>
+        <el-form ref="form" :model="form" >
+          <el-form-item label="账号">
+            <el-input v-model="account" placeholder="点击输入账号"></el-input>
+          </el-form-item>
+          <el-form-item label="密码">
+            <el-input v-model="password" placeholder="点击输入密码"></el-input>
+          </el-form-item>
+          <el-form-item :span="12">
+            <el-button type="primary" @click="onSubmit">登录</el-button>
+          </el-form-item>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<style lang="stylus" scoped>
+</style>
+<script>
+    export default{
+      data() {
+        return {
+          account: '',
+          password: ''
+        };
+      },
+      methods: {
+        onSubmit() {
+           console.log(1);
+        }
+      }
+    };
+</script>
---
+++ src/components/TodoList.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="todoList">
+    <el-row class="content">
+      <el-col :xs="{span:20,offset:2}" :sm="{span:8,offset:8}">
+        <span>欢迎{{name}}!,你的待办事项是：</span>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+<style lang="stylus" scoped>
+</style>
+<script>
+    export default{};
+</script>
